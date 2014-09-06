ccd3f5fb1096ddc948c42e940717d0d751971906
Aman Singh <netxillon@yahoo.com>
2014-09-06 17:02:13 +0530

    kerberos

---
+++ kerberos/Namenode/hadoop.sh
@@ -0,0 +1,11 @@
+export JSVC_HOME=/usr/bin
+export HADOOP_SECURE_DN_USER=hdfs
+
+export HADOOP_SECURE_DN_PID_DIR=/var/hadoop/pids
+export HADOOP_SECURE_DN_LOG_DIR=/var/hadoop/logs
+
+export JAVA_HOME=/usr/java/jdk1.8.0_11
+export HADOOP_HOME=/usr/local/hadoop
+export HADOOP_HOME_WARN_SUPPRESS="TRUE"
+
+PATH=$JAVA_HOME/bin:$HADOOP_HOME/bin:$PATH
